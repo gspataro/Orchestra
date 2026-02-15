3af2f0eaef0daa68d77859522d16247108c24b6f
Giuseppe Spataro <spatyx@gmail.com>
2026-02-15 21:22:12 +0100

    remove(Assets): remove old Media engine

--- src/Orchestra/Assets/AssetsComponent.php
+++ src/Orchestra/Assets/AssetsComponent.php
@@ -15,10 +15,6 @@ final class AssetsComponent extends Component
                 $args['outputPath']
             );
         });
-
-        $container->add('assets.media', function ($container, $args): object {
-            return new Media();
-        });
     }
 
     public function boot(Container $container): void
@@ -32,13 +28,5 @@ final class AssetsComponent extends Component
             'outputPath' => '/assets/'
         ]);
         $vite->loadManifest();
-
-        $media = $container->get('assets.media');
-
-        $media->addSize('thumbnail', 400, 0, 90);
-        $media->addSize('medium', 600, 0, 90);
-        $media->addSize('large', 900, 0, 90);
-        $media->addSize('full', 1920, 0, 90);
-        $media->addSize('original', 0, 0, 90);
     }
 }
