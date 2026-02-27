6b8a200aa26710d44c9ae09f85134d73a44156dd
Giuseppe Spataro <spatyx@gmail.com>
2026-02-27 15:52:02 +0100

    chore(Sitemap)

--- src/Orchestra/Project/Sitemap.php
+++ src/Orchestra/Project/Sitemap.php
@@ -4,45 +4,18 @@ namespace Orchestra\Project;
 
 final class Sitemap
 {
-    /**
-     * Store website sitemap
-     *
-     * @var array
-     */
-
+    /** @var array<string,string> */
     private array $sitemap = [];
-
-    /**
-     * Verify if a page exists in the sitemap
-     *
-     * @param string $tag
-     * @return bool
-     */
 
     public function has(string $tag): bool
     {
         return isset($this->sitemap[$tag]);
     }
 
-    /**
-     * Verify if a path exists in the sitemap
-     *
-     * @param string $path
-     * @return bool
-     */
-
     public function hasPath(string $path): bool
     {
         return in_array($path, array_values($this->sitemap));
     }
-
-    /**
-     * Add a path to the sitemap and return its unique version
-     *
-     * @param string $tag
-     * @param string $path
-     * @return string
-     */
 
     public function add(string $tag, string $path): string
     {
@@ -53,13 +26,6 @@ final class Sitemap
         $this->sitemap[$tag] = $path;
         return $path;
     }
-
-    /**
-     * Generate a unique path
-     *
-     * @param string $path
-     * @return string
-     */
 
     public function generateUniquePath(string $path): string
     {
@@ -74,23 +40,10 @@ final class Sitemap
         return $path;
     }
 
-    /**
-     * Get a path
-     *
-     * @param string $tag
-     * @return string|null
-     */
-
     public function get(string $tag): ?string
     {
         return $this->sitemap[$tag] ?? null;
     }
-
-    /**
-     * Get all the sitemap
-     *
-     * @return array
-     */
 
     public function getAll(): array
     {
