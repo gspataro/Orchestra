e681726cc33ce139a82ca4401c18967fd84c7cab
Giuseppe Spataro <spatyx@gmail.com>
2026-02-28 12:38:32 +0100

    chore(SpecificationInterface): fix phpdoc generics

--- src/Orchestra/Blueprint/SpecificationInterface.php
+++ src/Orchestra/Blueprint/SpecificationInterface.php
@@ -5,6 +5,15 @@ namespace Orchestra\Blueprint;
 interface SpecificationInterface
 {
     public function namespace(): string;
+
+    /**
+     * @return array<string,array<string,string|int|bool|array<string|int,mixed>>>
+     */
     public function definition(): array;
+
+    /**
+     * @param array<string|int,mixed> $data
+     * @return NamespaceInterface
+     */
     public function createNamespace(array $data): NamespaceInterface;
 }
