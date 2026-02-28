b2bf27e9964c7b78939e5266ad46ed0c4f031908
Giuseppe Spataro <spatyx@gmail.com>
2026-02-28 12:54:01 +0100

    chore(ContentPayload): fix php generics

--- src/Orchestra/Content/ContentPayload.php
+++ src/Orchestra/Content/ContentPayload.php
@@ -6,6 +6,11 @@ use Orchestra\Content\Source;
 
 final readonly class ContentPayload
 {
+    /**
+     * @param mixed $body
+     * @param array<string|int,mixed> $metadata
+     * @param Source $source
+     */
     public function __construct(
         public mixed $body,
         public array $metadata,
