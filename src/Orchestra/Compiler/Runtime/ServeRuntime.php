7424663efe958004e90f5d99dc491b38d374bcad
Giuseppe Spataro <spatyx@gmail.com>
2026-03-02 19:00:47 +0100

    chore(ServerRuntime)

--- src/Orchestra/Compiler/Runtime/ServeRuntime.php
+++ src/Orchestra/Compiler/Runtime/ServeRuntime.php
@@ -8,21 +8,13 @@ use Orchestra\Rehearsal\Router;
 
 final class ServeRuntime extends Runtime
 {
-    private PageCollection $pages;
     private Router $router;
 
     public function run(BuildOptions $options): bool
     {
-        if ($options->cleanupOnly) {
-            $this->output->warning('Skipping pages build');
-            return true;
-        }
+        $this->output->info('Serving pages');
 
-        $this->output->info('Building pages');
-
-        $this->pages = $this->container->get('pages.collection');
         $this->router = $this->container->get('rehearsal.router');
-
         $this->router->handleRequest($_SERVER);
 
         return true;
