2b03dded11c4ce53f495def03924029bbdf6e9ee
Giuseppe Spataro <spatyx@gmail.com>
2026-03-10 10:01:25 +0100

    chore(BaseReader): remove unused methods

--- src/Orchestra/Content/Reader/BaseReader.php
+++ src/Orchestra/Content/Reader/BaseReader.php
@@ -8,17 +8,6 @@ use Orchestra\Content\Source;
 
 abstract class BaseReader implements ReaderInterface
 {
-    protected function generateContentId(Source $source): mixed
-    {
-        return sha1($source->group . ':' . $source->relativePath);
-    }
-
-    protected function generateContentTag(Source $source): mixed
-    {
-        $fileName = pathinfo($source->relativePath, PATHINFO_FILENAME);
-        return $source->group . '.' . $fileName;
-    }
-
     /**
      * @param Source $source
      * @param mixed $body
