80270ab24d92945e48ef39259d4847fe449879d6
Giuseppe Spataro <spatyx@gmail.com>
2026-03-16 12:26:35 +0100

    update(CacheCleanCommand): add view-only option

--- src/Orchestra/Console/Command/CacheCleanCommand.php
+++ src/Orchestra/Console/Command/CacheCleanCommand.php
@@ -7,6 +7,16 @@ final class CacheCleanCommand extends BaseCommand
     protected string $name = 'cache:clean';
     protected ?string $description = 'Delete build cache';
 
+    public function options(): array
+    {
+        return [
+            'view-only' => [
+                'type' => 'toggle',
+                'description' => 'Delete only the theme cache'
+            ]
+        ];
+    }
+
     public function main(): void
     {
         $this->output->print('{bold}Cleaning up cache...');
@@ -14,11 +24,16 @@ final class CacheCleanCommand extends BaseCommand
         /** @var \Orchestra\Compiler\Factory\BuildContextFactory */
         $contextFactory = $this->container->get('compiler.context.factory');
         $context = $contextFactory->make();
+        $excluded = [];
+
+        if ($this->argument('view-only') !== null) {
+            $excluded[] = $context->paths()->cache('orchestra');
+        }
 
         recursiveDelete(
             $context->paths()->cache(),
             false,
-            []
+            $excluded
         );
 
         $this->output->print("{bold}{fg_green}Cache cleand.");
