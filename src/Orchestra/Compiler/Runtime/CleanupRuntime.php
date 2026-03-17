c9a7a47ce3be79b648099937b2b07bd2687f0da4
Giuseppe Spataro <spatyx@gmail.com>
2026-03-17 16:08:54 +0100

    fix(CleanupRuntime): media shouldn't be deleted

--- src/Orchestra/Compiler/Runtime/CleanupRuntime.php
+++ src/Orchestra/Compiler/Runtime/CleanupRuntime.php
@@ -17,7 +17,7 @@ final class CleanupRuntime extends Runtime
 
         $excluded = array_merge(
             $this->context->prototype()->configs()->get('orchestra.cleanup'),
-            ['.htaccess']
+            ['.htaccess', 'media']
         );
 
         foreach ($outputDirectory as $item) {
